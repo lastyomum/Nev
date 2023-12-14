6419145f2fab0fd388d5255591d36761e5751065
lastyomum <106666128+lastyomum@users.noreply.github.com>
2023-12-14 19:54:09 +1100

    first commit

--- js/mobile.js
+++ js/mobile.js
@@ -18,21 +18,20 @@ function detectMob() {
 function yass() {
     if(window.innerWidth <= 500) {
         var nfs = document.getElementById("nf");
-        nfs.style.width = "275pt"
         var infoBar = document.getElementById("info");
-        infoBar.style.width = "100pt"
-        infoBar.style.borderRadius = "7px"
+        infoBar.style.width = "100pt";
+        infoBar.style.borderRadius = "7px";
         var input = document.getElementById("ins");
-        input.style.width = "100pt";
     }
 
     else if(window.innerWidth >= 500) {
         var nfs = document.getElementById("nf");
-        nfs.style.width = "1120pt"
         var infoBar = document.getElementById("info");
-        infoBar.style.width = "1300pt"
-        infoBar.style.borderRadius = "0px"
+        infoBar.style.width = "1300pt";
+        infoBar.style.borderRadius = "0px";
+        var input = document.getElementById("ins");
     }
-}
+    }
+
 
 setInterval(yass,1000)
